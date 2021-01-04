0338d7fe9362dce54499cf87e3fc4dcdfd7a069b
wadkan <zsoltgaramszegi@gmail.com>
2021-01-04 16:21:13 +0100

    worked, before install react

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import Header from './components/layout/Header'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import {v4 as uuid} from "uuid"; 
+
+import './App.css';
+
+class App extends Component{
+state = {
+  todos: [
+    {
+      id: uuid(),
+      title: 'Take out the trask',
+      completed: false
+    },
+    {
+      id: uuid(),
+      title: 'Dinner with wife',
+      completed: false
+    },
+    {
+      id: uuid(),
+      title: 'Meeting with boss',
+      completed: false
+    },
+    
+  ]
+}
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    } )})
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  // AddTodo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete}
+          delTodo={this.delTodo}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
